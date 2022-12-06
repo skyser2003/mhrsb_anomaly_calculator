be5f8717b652517a5fd10b9ce58a4e2d0971f9df
Seung Ho Jang <shjang1992@gmail.com>
2022-12-06 20:49:44 +0900

    Unused types removed

--- calculator/rust_workspace/calculator/src/calc/types.rs
+++ calculator/rust_workspace/calculator/src/calc/types.rs
@@ -4,7 +4,7 @@ use nalgebra::SVector;
 
 use super::{
     calc_equipment::CalcEquipment,
-    constant::{CALC_VEC_LENGTH, EQUIP_PART_COUNT, MAX_SLOT_LEVEL, SKILLS_COUNT},
+    constant::{EQUIP_PART_COUNT, MAX_SLOT_LEVEL, SKILLS_COUNT},
 };
 
 pub type SkillSlotCount = i8;
@@ -15,7 +15,5 @@ pub type SkillLevelVec = SVector<SkillSlotCount, SKILLS_COUNT>;
 pub type SkillsTuple = Vec<(usize, SkillSlotCount)>;
 pub type SlotsVec = SVector<SkillSlotCount, MAX_SLOT_LEVEL>;
 pub type PointsVec = SVector<i32, MAX_SLOT_LEVEL>;
-
-pub type CalcInternalVec = SVector<i16, { CALC_VEC_LENGTH }>;
 
 pub type EquipmentsArray<'a> = [&'a Arc<CalcEquipment>; EQUIP_PART_COUNT];
